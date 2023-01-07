ef008adf972434f6af4b9048ce246f50809dae6c
Daniel Mertins <dalvesmertins@gmail.com>
2023-01-07 15:58:06 -0300

    Add test for Category non-null name constraint

--- domain/src/test/java/com/dmertins/videocatalogadmin/domain/category/CategoryTest.java
+++ domain/src/test/java/com/dmertins/videocatalogadmin/domain/category/CategoryTest.java
@@ -1,5 +1,7 @@
 package com.dmertins.videocatalogadmin.domain.category;
 
+import com.dmertins.videocatalogadmin.domain.exceptions.DomainException;
+import com.dmertins.videocatalogadmin.domain.validation.handler.ThrowsValidationHandler;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,5 +23,21 @@ public class CategoryTest {
         assertNotNull(actualCategory.getCreatedAt());
         assertNotNull(actualCategory.getUpdatedAt());
         assertNull(actualCategory.getDeletedAt());
+    }
+
+    @Test
+    public void shouldRaiseNullNameError() {
+        final String expectedName = null;
+        final var expectedDescription = "The movies category";
+        final var expectedIsActive = true;
+        final var expectedErrorCount = 1;
+        final var expectedErrorMessage = "'name' should not be null";
+
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        final var actualException =
+                assertThrows(DomainException.class, () -> actualCategory.validate(new ThrowsValidationHandler()));
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0).message());
     }
 }
